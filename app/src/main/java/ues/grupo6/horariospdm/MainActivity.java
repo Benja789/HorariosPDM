ddf0fbd2881190b15c485450b3c91d94c47f2cb0
Benjamin Molina <molina.benjamin70@gmail.com>
2024-05-01 18:45:56 -0600

    Avances en el menu, diseño y configuracion inicial

--- app/src/main/java/ues/grupo6/horariospdm/MainActivity.java
+++ app/src/main/java/ues/grupo6/horariospdm/MainActivity.java
@@ -2,23 +2,31 @@ package ues.grupo6.horariospdm;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        initToolBar();
+    }
+
+    private void initToolBar () {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar_element);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.main);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout, R.string.open_menu,R.string.closed_menu);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
