3c5f1d0373123d8297a0d7ba470a4ae3b31dd8ee
Benjamin Molina <molina.benjamin70@gmail.com>
2024-06-12 00:38:36 -0600

    Metodo para crear eventos

--- app/src/main/java/ues/grupo6/horariospdm/evento/EventoInsertarActivity.java
+++ app/src/main/java/ues/grupo6/horariospdm/evento/EventoInsertarActivity.java
@@ -13,18 +13,22 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import ues.grupo6.horariospdm.ControlBD;
 import ues.grupo6.horariospdm.R;
-import ues.grupo6.horariospdm.tipo_evento.TipoEvento;
 
 public class EventoInsertarActivity extends Activity {
 
@@ -33,8 +37,6 @@ public class EventoInsertarActivity extends Activity {
     Spinner spTipoEventos;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-
-    //@SuppressLint("MissingInflatedId")
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,76 +45,58 @@ public class EventoInsertarActivity extends Activity {
         helper = new ControlBD(this);
         edNombreEvento = (EditText) findViewById(R.id.editNombreEvento);
         spTipoEventos = (Spinner) findViewById(R.id.spinnerTipoEvento);
-
-        // Cargar tipos de eventos desde la base de datos
         helper.abrir();
-        cargarTiposEventosActivos();
         getData();
 
     }
 
-    private void cargarTiposEventosActivos() {
-        ArrayList<TipoEvento> listaTipoEventos = new ArrayList<TipoEvento>();
-        listaTipoEventos = helper.consultarTiposEventosActivos();
-
-
-
-        // Crear un ArrayList de Strings con los nombres de los tipos de eventos
-        ArrayList<String> nombresTipoEventos = new ArrayList<>();
-        for (TipoEvento tipoEvento : listaTipoEventos) {
-            nombresTipoEventos.add(tipoEvento.getNombre_tipo_evento());
-        }
-
-        // Crear ArrayAdapter con los nombres de los tipos de eventos
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, nombresTipoEventos);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spTipoEventos.setAdapter(adapter);
-
-    }
-
     public void insertarEvento(View v) {
-        String nombre = edNombreEvento.getText().toString();
+        // Obtén una referencia a la colección de Firestore
+        final CollectionReference myRef = db.collection("evento");
 
-        // Obtener el nombre del tipo de evento seleccionado en el Spinner
-        String nombreTipoEvento = spTipoEventos.getSelectedItem().toString();
+        // Obtén una referencia al documento que deseas referenciar
+        db.collection("tipo_evento")
+                .whereEqualTo("nombre_tipo_evento", spTipoEventos.getSelectedItem().toString())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                DocumentReference tipoEventoRef = document.getReference();
 
-        // Buscar el objeto TipoEvento correspondiente al nombre seleccionado
-        TipoEvento tipoEventoSeleccionado = buscarTipoEventoPorNombre(nombreTipoEvento);
+                                // Crea un nuevo documento con los datos que deseas guardar
+                                Map<String, Object> data = new HashMap<>();
+                                data.put("nombre_evento", edNombreEvento.getText().toString());
+                                data.put("estado_evento", true);
+                                data.put("id_tipo_evento", tipoEventoRef);
 
-        // Verificar si se encontró el tipo de evento
-        if (tipoEventoSeleccionado != null) {
-            int id_TipoEvento = tipoEventoSeleccionado.getId_tipo_evento();
-            Evento evento = new Evento();
-            evento.setNombre_evento(nombre);
-            evento.setId_tipo_evento(id_TipoEvento);
-            evento.setEstado_evento(1);
-
-            helper.abrir();
-            String regInsertados = helper.insertarEvento(evento);
-            helper.cerrar();
-
-            Toast.makeText(this, regInsertados, Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "Error: Tipo de evento no encontrado", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private TipoEvento buscarTipoEventoPorNombre(String nombreTipoEvento) {
-        ArrayList<TipoEvento> listaTipoEventos = helper.consultarTiposEventosActivos();
-
-        for (TipoEvento tipoEvento : listaTipoEventos) {
-            if (tipoEvento.getNombre_tipo_evento().equals(nombreTipoEvento)) {
-                return tipoEvento;
-            }
-        }
-        // Retornar null si el tipo de evento no se encontró
-        return null;
+                                // Agrega el nuevo documento a la colección
+                                myRef.add(data)
+                                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                            @Override
+                                            public void onSuccess(DocumentReference documentReference) {
+                                                Toast.makeText(EventoInsertarActivity.this, "Evento insertado", Toast.LENGTH_SHORT).show();
+                                                limpiarTexto(v);
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.w("TAG", "Error adding document", e);
+                                            }
+                                        });
+                            }
+                        } else {
+                            Log.d("TAG", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
 
     public void limpiarTexto(View v) {
         edNombreEvento.setText("");
     }
-
 
     public void getData() {
         CollectionReference myRef = db.collection("tipo_evento");
